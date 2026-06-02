a734584b90c765356a2aeeb981adad18d81142a6
algrel <alinagrel2006@gmail.com>
2026-06-02 21:40:39 +0300

    Update part_by_term.py

--- scripts/part_by_term.py
+++ scripts/part_by_term.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-type = {
+types = {
     'МСП до 1 года': 'МСП',
     'МСП свыше 1 года': 'МСП',
     'Нефин до 1 года': 'Нефин',
@@ -9,7 +9,7 @@ type = {
     'ФЛ свыше 1 года': 'ФЛ'
 }
 
-term = {
+terms = {
     'МСП до 1 года': 'До 1 года',
     'МСП свыше 1 года': 'Свыше 1 года',
     'Нефин до 1 года': 'До 1 года',
@@ -22,7 +22,7 @@ term = {
 df = pd.read_excel('part_by_term_before.xlsx', sheet_name='Chart data')
 df['Месяц'] = pd.to_datetime(df['Месяц'])
 result = pd.melt(df, id_vars=['Месяц'], var_name='Категория', value_name='Соотношение')
-result['Тип_заемщика'] = result['Категория'].map(type)
-result['Срок_кредита'] = result['Категория'].map(term)
+result['Тип_заемщика'] = result['Категория'].map(types)
+result['Срок_кредита'] = result['Категория'].map(terms)
 result = result.sort_values(['Месяц', 'Категория']).reset_index(drop=True)
 result.to_excel('part_by_term_after.xlsx', index=False)
