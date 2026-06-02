baf2e10c22b8fd0a349863566b8fad4a7361af2b
algrel <alinagrel2006@gmail.com>
2026-06-02 21:42:47 +0300

    Update okved.py

--- scripts/okved.py
+++ scripts/okved.py
@@ -1,19 +1,19 @@
 import pandas as pd
 import re
 
-def type(s):
+def get_type(s):
     if s.startswith('МСП'):
         return 'МСП'
     elif s.startswith('Нефин'):
         return 'Нефин'
 
-def term(s):
+def get_term(s):
     if 'до 1 года' in s:
         return 'До 1 года'
     elif 'свыше 1 года' in s:
         return 'Свыше 1 года'
 
-def okved(s):
+def get_okved(s):
     res = re.sub(r'^(МСП|Нефин)\s+', '', s)
     res = re.sub(r'\s+(до|свыше)\s+1\s+года$', '', s)
     res = re.sub(r'^[A-ZА-Я]\.\s*', '', s)
@@ -23,9 +23,9 @@ def okved(s):
 df = pd.read_excel('okved_before.xlsx', sheet_name='Chart data')
 df['Месяц'] = pd.to_datetime(df['Месяц'])
 result = pd.melt(df, id_vars=['Месяц'], var_name='Категория', value_name='Ставка')
-result['Тип_заемщика'] = result['Категория'].apply(type)
-result['Срок_кредита'] = result['Категория'].apply(term)
-result['ОКВЭД'] = result['Категория'].apply(okved)
+result['Тип_заемщика'] = result['Категория'].apply(get_type)
+result['Срок_кредита'] = result['Категория'].apply(get_term)
+result['ОКВЭД'] = result['Категория'].apply(get_okved)
 result = result.drop('Категория', axis=1)
 result = result[['Месяц', 'Тип_заемщика', 'ОКВЭД', 'Срок_кредита', 'Ставка']].sort_values(['Месяц', 'Тип_заемщика', 'ОКВЭД', 'Срок_кредита']).reset_index(drop=True)
 result.to_excel('okved_after.xlsx', index=False)
